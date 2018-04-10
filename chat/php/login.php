1c243cab924f31a31b411d130576694d6def92c1
Steppo <stefanolorini1999@gmail.com>
2018-04-10 13:59:54 +0200

    roba

---
+++ chat/php/login.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    if(!isset($_POST['username']) || !isset($_POST['password'])){
+        header('Location: /chattazza/index.html');
+        die();
+    }  
+    else{
+        if(isset($_POST['login']))
+            echo "Login";
+        if(isset($_POST['sign_up']))
+            echo "Signup";
+    }
+?>
