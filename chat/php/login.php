b347d4c60808fe53b867487ca3b198a90a7fe34a
Steppo <stefanolorini1999@gmail.com>
2018-04-24 11:31:21 +0200

    cose

--- chat/php/login.php
+++ chat/php/login.php
@@ -8,7 +8,16 @@
         if(isset($_POST['login']))
         {
             echo "Login";
-            
+            $host="127.0.0.1";
+            $user=$_POST["username"];
+            $password=$_POST["password"];
+            /* connessione */
+            $connection=mysqli_connect($host
+            6,$user,$password);
+            or die("errore di connessione");
+            /* selezione db */
+            mysqli_select_db($connection,"chat") or die("errore nella selezione del db");
+            mysqli_close($connection);
         }
         if(isset($_POST['sign_up']))
             header('Location: /chattazza/chat/html/signup.html');
